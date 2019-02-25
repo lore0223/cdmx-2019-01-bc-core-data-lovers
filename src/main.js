0f43e16194e8e4d56653e022bd6d99707c05d6a7
lore0223 <loremma23@gmail.com>
2019-02-25 16:50:10 -0600

    Ya conseguí que todas las funciones estén renderizando la data. No sé si es la manera correcta pero  ya funciona toda la página con fetch.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 //const data = window.POKEMON.pokemon; //trae la data//
+let dataJson= [];
 const initialPage = document.getElementById('initial-page'); //pagina de inicio//
 const pokemonPage = document.getElementById('pokemon-page'); //página de pokemones//
 const initialPokeballButton = document.getElementById('pokeball-button');
@@ -13,31 +14,33 @@ const aside = document.getElementById('aside');
 
 initialPokeballButton.addEventListener('click', () => { // ocultar página de inicio//
   initialPage.classList.add('hideElement');
-   pokemonPage.classList.remove('hideElement');
-  });
+  pokemonPage.classList.remove('hideElement');
+});
+
+
+fetch('./data/pokemon/pokemon.json')
+.then(response => response.json())
+.then(pokemonJson => {
+  const arrData=pokemonJson.pokemon;
+  const dataPokemon=window.pokesaurius.getDataPokemon(arrData);
+  dataJson=dataPokemon;
+  print(dataJson)
+  return dataJson
+})
+.then(newData => filterByPokemon(newData))
+ .catch(err => console.error(err))
+
+console.log(dataJson)
+// const fetchPokemon = () => { 
+//   
+// };
+// fetchPokemon()
 
   
-  
-  
-  fetch('./data/pokemon/pokemon.json')
-  .then(response => response.json())
-  .then(pokemonJson => {
-    const arrData=pokemonJson.pokemon;
-    const data=window.pokesaurius.getDataPokemon(arrData);
-    localStorage.setItem(data,JSON.stringify(arrData))
-    print(data)
-    return data
-  })
-   .then(newData => filterByPokemon(newData))
-  // .then()
-  
-  
-  .catch(err => console.error(err))
-  
 
-  const print = (data) => { //imprime la data//
+  const print = (dataJson) => { //imprime la data//
     let totalWeight = 0; //Contador que  guarda el peso de los pokemon
-    data.forEach(element => {
+    dataJson.forEach(element => {
       totalWeight += parseFloat(element.weight);
       let result = `<div id="${element.id}" class="wrapper">
       <div class="pokemon-card"> 
@@ -53,39 +56,40 @@ initialPokeballButton.addEventListener('click', () => { // ocultar página de in
       printList.insertAdjacentHTML("beforeend", result); //insertAdjacentHTML renderiza cada iteración y  coloca los elementos uno después del otro.
       return print;
     });
-    const averageWeight = totalWeight / data.length; //saca promedio del peso//
+    const averageWeight = totalWeight / dataJson.length; //saca promedio del peso//
     weight.innerHTML = averageWeight.toFixed(2); //To Fixed 2 te da solo 2 decimales del resultado  final de la división
-    counterCollection.innerHTML = data.length;
+    counterCollection.innerHTML = dataJson.length;
   };
   
 
   const menuUndo = document.getElementById('menu-undo');  
-    menuUndo.addEventListener('click', () => {
-    printList.innerHTML ='';
+  const homme = menuUndo.addEventListener('click', () => {
+   printList.innerHTML ='';
+   print(dataJson)
     
-    print(data)
     });
   
     
-const  filterByPokemon = (data) => {
+const  filterByPokemon = (dataJson) => {
   for (let i = 0; i < buttonFilterByType.length; i++) {
     buttonFilterByType[i].addEventListener('click', () => {
       aside.classList.add('hideElement'); //oculta el aside en versión ipad y mobile//
       let pokemonElegido = buttonFilterByType[i].id //esta let guarda la elección del usuario por medio del click y nos trae la posición y el id del boton.
       printList.innerHTML = ''; //limpia la página antes de renderizar los pokémon filtrados
-      const pokemonFiltrados = window.pokesaurius.typeFilter(data, pokemonElegido); //es la invocación de la función que filtra
+      const pokemonFiltrados = window.pokesaurius.typeFilter(dataJson, pokemonElegido); //es la invocación de la función que filtra
       print(pokemonFiltrados)
       printType.innerHTML = pokemonElegido; //imprime el id en la seccion de sabias que?
     });
   }
 }
 
-  alphabeticOrderButton.addEventListener('click', () => { //función que invoca ala función de ordenar A-Z y la renderiza
-    printList.innerHTML = '';
-    const resultSortByName = window.pokesaurius.sortByName(data);
+
+    alphabeticOrderButton.addEventListener('click', () => { //función que invoca ala función de ordenar A-Z y la renderiza
+      printList.innerHTML ='';
+    const resultSortByName = window.pokesaurius.sortByName(dataJson);
     print(resultSortByName);
-  
   });
+  
 
   menuToggle.addEventListener('click', () => {
    aside.classList.remove('hideElement');
@@ -109,3 +113,6 @@ const  filterByPokemon = (data) => {
   //local storage puede guardar un string solo acepta strings json.stringyfy ()los convierte 
   //localStorage.setItem('data,JSON.stringify(arrData))mandar el dato
   //localStorage.getItem('data')
+
+  //variables, funciones declaradas, funciones invocadas, eventos add eventlistener 
+  
